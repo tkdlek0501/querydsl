667da48037e2eb07c2a0a8014a4813a0554b23d6
tkdlek0501 <tkdlek0501@naver.com>
2022-07-18 21:40:52 +0900

    QuerydslRepositorySupport 정리

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -10,19 +10,24 @@ import javax.persistence.EntityManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.util.StringUtils;
 
 import com.querydsl.core.QueryResults;
-import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
 
-public class MemberRepositoryImpl implements MemberRepositoryCustom {
+public class MemberRepositoryImpl implements MemberRepositoryCustom{
+	// extends QuerydslRepositorySupport
+	
+//	public MemberRepositoryImpl(Class<?> domainClass) {
+//		super(Member.class);
+//	}
 	
 	private final JPAQueryFactory query;
 	
@@ -32,6 +37,26 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
 	
 	@Override
 	public List<MemberTeamDto> search(MemberSearchCondition condition){
+		
+		// QuerydslRepositorySupport 의 지원을 받으면 아래 형식으로 사용
+		// sort 가 정상 작동하지 않는다는 치명적인 단점때문에 사용 x
+//		from(member)
+//		.leftJoin(member.team, team)
+//		.where(
+//				usernameEq(condition.getUsername()),
+//				teamNameEq(condition.getTeamName()),
+//				ageGoe(condition.getAgeGoe()),
+//				ageLoe(condition.getAgeLoe())
+//				)
+//		.select(new QMemberTeamDto(
+//				member.id.as("memberId"), // MemberTeamDto 에서 받을 필드명을 as로
+//				member.username,
+//				member.age,
+//				team.id.as("teamId"),
+//				team.name.as("teamName")
+//			))
+//		.fetch();
+		
 		return query
 				.select(new QMemberTeamDto(
 							member.id.as("memberId"), // MemberTeamDto 에서 받을 필드명을 as로
